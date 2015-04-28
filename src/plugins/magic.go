a2c12b5a57ca34a72eb3fa876f4b391e68da912b
Splizard <quentin@squeezedoranges.com>
2015-04-28 23:19:46 +1200

    Update magic plugin to use magiccards.info

--- src/plugins/magic.go
+++ src/plugins/magic.go
@@ -1,20 +1,23 @@
 //Magic.
-//This plugin will download magic cards from mtgimage.com
+//This plugin will download magic cards from http://magiccards.info
 package plugins
 
-import (
-	"fmt"
-	"net/http"
-	"errors"
-	"os"
-	"io"
-)
+import "fmt"
+import "net/http"
+import "net/url"
+import "errors"
+import "os"
+import "regexp"
+import "io"
+import "io/ioutil"
 
 const Magic = "magic"
 
 func init() {
 
 	var client http.Client
+	
+	var magicimageregex *regexp.Regexp = regexp.MustCompile(`http://magiccards.info/scans/([0-9a-zA-z \/_\.\-,:]*)`)
 
 	RegisterHeaders(Magic, []string{"Magic: The Gathering", "Magic", "MTG"})
 	
@@ -22,38 +25,62 @@ func init() {
 
 	RegisterPlugin(Magic, func(name, info string, detecting bool) string {
 
-		if !detecting {
-			fmt.Println("getting", "http://mtgimage.com/card/"+name+".jpg")
+		var search string
+		var imagename string = name
+
+		if _, err := os.Stat( DeckerCachePath + "/cards/magic/" + name + ".jpg"); !os.IsNotExist(err) {
+			return Magic
+		}
+		
+		search = "http://magiccards.info/query?q="+url.QueryEscape(name)+"&v=card&s=cname"
+		
+		response, err := client.Get(search)
+		Handle(err)
+		
+		if response.StatusCode != 200 {
+			//Not sure what happens here.
+			fmt.Println("possible error check card! " + name + ", status " + response.Status)
+		}
+		
+		body, err := ioutil.ReadAll(response.Body)
+		Handle(err)
+		
+		//Magic Image Regex!
+		submatches := magicimageregex.FindStringSubmatch(string(body))
+		if len(submatches) < 2 {
+			//Indeed.. a bug on magiccards.info :3
+			Handle(errors.New("No image found for card " + name + ", this could be a bug !"))
+		}
+		image := "http://magiccards.info/scans/"+string(submatches[1])
+
+		//Now we can check if we already have the image cached, otherwise download it.
+		if _, err := os.Stat(DeckerCachePath  + "/cards/magic/" + name + ".jpg"); !os.IsNotExist(err) {
+			return Magic
 		} else {
-			if _, err := os.Stat( DeckerCachePath + "/cards/magic/" + name + ".jpg"); !os.IsNotExist(err) {
+			if ! detecting {
+				fmt.Println("getting", image)
+			}
+			response, err = client.Get(image)
+			Handle(err)
+			if response.StatusCode == 404 {
+				//Broken link?
+				Handle(errors.New("broken link? " + image))
+			} else {
+				if response.StatusCode != 200 {
+					fmt.Println("possible error check file! " + DeckerCachePath + "/cards/magic/" + imagename + ".jpg, status " + response.Status)
+				}
+				//Download and Save image.
+				var imageOut *os.File
+				if info != "" {
+					imageOut, err = os.Create(DeckerCachePath + "/cards/magic/" + imagename + ".jpg")
+				} else {
+					imageOut, err = os.Create(DeckerCachePath + "/cards/magic/" + name + ".jpg")
+				}
+				Handle(err)
+				io.Copy(imageOut, response.Body)
+				imageOut.Close()
 				return Magic
 			}
-		}
-
-		//For magic cards it is easy we just request the name from mtgimage.com and tada! we have an image.
-		response, err := client.Get("http://mtgimage.com/card/" + name + ".jpg")
-		Handle(err)
-
-		//Unless we get a 404 which means the name wasn't given correctly.
-		if response.StatusCode == 404 {
-			if !detecting {
-				//Complain about it.
-				Handle(errors.New("card name '" + name + "' seems to be invalid!\nCheck http://mtgimage.com/card/" + name + ".jpg"))
-			} else {
-				//Or it just means this is not a magic deck.
-				return None
-			}
-		} else {
-			if response.StatusCode != 200 {
-				//Hmmm why is the status code not 200?
-				println("possible error check file! " + DeckerCachePath + "/cards/magic/" + name + ".jpg, status " + response.Status)
-			}
-			//Download and Save image.
-			imageOut, err := os.Create(DeckerCachePath + "/cards/magic/" + name + ".jpg")
-			Handle(err)
-			io.Copy(imageOut, response.Body)
-			imageOut.Close()
-			return Magic
 		}
 		return None
 	})
---
+++ src/plugins/mtgimage.go
@@ -0,0 +1,65 @@
+// +build ignore
+
+//Magic.
+//This plugin will download magic cards from mtgimage.com
+//
+//Sadly mtgimage.com was shutdown by Wizards of the Coast and therefore is no longer available, for reference purposes I have kept this file.
+//Maybe somebody can recreate mtgimage.com on the dark web.
+package plugins
+
+import (
+	"fmt"
+	"net/http"
+	"errors"
+	"os"
+	"io"
+)
+
+const Magic = "magic"
+
+func init() {
+
+	var client http.Client
+
+	RegisterHeaders(Magic, []string{"Magic: The Gathering", "Magic", "MTG"})
+	
+	RegisterBack(Magic, "http://mtgimage.com/card/cardback.hq.jpg")
+
+	RegisterPlugin(Magic, func(name, info string, detecting bool) string {
+
+		if !detecting {
+			fmt.Println("getting", "http://mtgimage.com/card/"+name+".jpg")
+		} else {
+			if _, err := os.Stat( DeckerCachePath + "/cards/magic/" + name + ".jpg"); !os.IsNotExist(err) {
+				return Magic
+			}
+		}
+
+		//For magic cards it is easy we just request the name from mtgimage.com and tada! we have an image.
+		response, err := client.Get("http://mtgimage.com/card/" + name + ".jpg")
+		Handle(err)
+
+		//Unless we get a 404 which means the name wasn't given correctly.
+		if response.StatusCode == 404 {
+			if !detecting {
+				//Complain about it.
+				Handle(errors.New("card name '" + name + "' seems to be invalid!\nCheck http://mtgimage.com/card/" + name + ".jpg"))
+			} else {
+				//Or it just means this is not a magic deck.
+				return None
+			}
+		} else {
+			if response.StatusCode != 200 {
+				//Hmmm why is the status code not 200?
+				println("possible error check file! " + DeckerCachePath + "/cards/magic/" + name + ".jpg, status " + response.Status)
+			}
+			//Download and Save image.
+			imageOut, err := os.Create(DeckerCachePath + "/cards/magic/" + name + ".jpg")
+			Handle(err)
+			io.Copy(imageOut, response.Body)
+			imageOut.Close()
+			return Magic
+		}
+		return None
+	})
+}
